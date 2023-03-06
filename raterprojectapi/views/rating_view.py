6284812432fde5142f6dfee93bf6ab0ee6fa1d1e
Matthew Hedges <matthedges35@gmail.com>
2023-03-06 14:54:19 -0600

    update complete

--- raterprojectapi/views/rating_view.py
+++ raterprojectapi/views/rating_view.py
@@ -2,3 +2,21 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
+from raterprojectapi.models import Rating, Gamer, Game
+
+class RatingView(ViewSet):
+
+    def update(self, request, pk):
+        rating = Rating.objects.get(pk=pk)
+        rating.rating = request.data["rating"]
+        rating.review = request.data["review"]
+        rating.date_created = request.data["date_created"]
+
+        game = Game.objects.get(pk=request.data["game"])
+        rating.game = game
+
+        gamer = Gamer.objects.get(pk=request.data["gamer"])
+        rating.gamer = gamer
+        rating.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
