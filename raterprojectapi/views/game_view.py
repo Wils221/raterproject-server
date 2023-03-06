d1cf80c956f2b45b88a2c48c7431bef006fdef1a
Grace Boaz <anna.g.boaz@gmail.com>
2023-03-06 13:27:35 -0600

    migrated fixtures. added a class to game_view. created an update funtion for game_view. also imported Game and Gamer from models into game_view

--- raterprojectapi/views/game_view.py
+++ raterprojectapi/views/game_view.py
@@ -2,3 +2,25 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
+from raterprojectapi.models import Game, Gamer
+
+class gameView(ViewSet):
+    """ This is the view for games """
+
+#UPDATE 
+    def update(self, request, pk):
+        """Handle PUT requests for a game
+        Returns:
+            Response -- Empty body with 204 status code 
+        """
+        game = Game.objects.get(pk=pk)
+
+        game.title = request.data["title"]
+        game.description = request.data["description"]
+        game.release_date = request.data["release_date"]
+
+        gamer = Gamer.objects.get(pk=request.data["organizer"])
+        game.gamer = gamer
+
+        game.save()
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
