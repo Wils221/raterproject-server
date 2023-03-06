7309cc9c49ed953d24ef9fd1fcb2707367ad0908
Wil Hammond <wil.hammond@gmail.com>
2023-03-06 13:58:13 -0600

    GameView, Retrieve, List, Create done

--- raterproject/urls.py
+++ raterproject/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from raterprojectapi.views import register_user, login_user
+from django.conf.urls import include
+from rest_framework import routers
+from raterprojectapi.views import GameView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'games', GameView, 'game')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register', register_user),
+    path('login', login_user),
+    path('', include(router.urls))
 ]
